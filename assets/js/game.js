503c9b537d5d87585f2c11814b73ab2bc3d2a085
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-05-23 21:39:17 -0400

    while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,9 +25,13 @@ var enemyAttack = 12;
 
 var fight = function (enemyName) {
 
+// repeat and execute as long as the enemy-robot is alive
+
+while(enemyHealth > 0) {
+
     // Alert players that they are starting the round
 
-    window.alert("Welcome to Robot Gladiators!");
+
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -114,9 +118,14 @@ fight();
 
 };
 
+}
 
 
 for (var i = 0; i < enemyNames.length; i++) {
+
+    var pickedEnemyName = enemyNames[i];
+
+    enemyHealth = 50;
 
 fight(enemyNames[i]);
 
